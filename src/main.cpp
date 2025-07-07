1d2bbff3379a25f8fb8eb3a147a15e42b98ec9ae
cadaeic628 <sebmorales@ug.uchile.cl>
2025-07-07 00:41:39 -0400

    feat: main file of the function created

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/*  main.cpp – Prototipo consola "Eolian Data Acquisition"
+ *  Autor: Sebastián Morales
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hola mundo!" << std::endl;
+    return 0;
+}
